d0c64b3d579399837e9cdc3d25e601f7668beeb1
hicham4i <hicham.rabai@outlook.fr>
2023-06-22 00:54:13 +0100

    fix url

--- src/components/body.vue
+++ src/components/body.vue
@@ -1,165 +1,190 @@
 <template>
   <div class="row">
     <div class="col-md-6">
-      <SectionRS v-for="(section, index) in sections.filter(x => !x.right)" v-bind:section="section" v-bind:key="'section-left-' + index"/>
+      <SectionRS
+        v-for="(section, index) in sections.filter((x) => !x.right)"
+        v-bind:section="section"
+        v-bind:key="'section-left-' + index"
+      />
     </div>
     <div class="col-md-6">
-      <SectionRS v-for="(section, index) in sections.filter(x => x.right)" v-bind:section="section" v-bind:key="'section-right-' + index"/>
+      <SectionRS
+        v-for="(section, index) in sections.filter((x) => x.right)"
+        v-bind:section="section"
+        v-bind:key="'section-right-' + index"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import SectionRS from './section.vue'
+import SectionRS from "./section.vue";
 
 export default {
-  name: 'BodyRS',
+  name: "BodyRS",
   components: { SectionRS },
-  data(){
+  data() {
     return {
-        sections: [
+      sections: [
+        {
+          right: false,
+          title: "EXPERIENCE",
+          items: [
             {
-                right: false,
-                title: 'EXPERIENCE',
-                items: [
-                    {
-                        timeperiod: '08/2019 → CURRENT',
-                        title: 'DataFlex',
-                        descriptions: [
-                            'Remote Full Stack Developer for <u><a href="https://qrmenu.com//">QRmenu</a></u> <strong>(Angular, AWS Lambda, NodeTs)</strong>.',
-                            'Remote Full Stack Developer for <u><a href="https://dopweb.com/">Dopweb</a></u> <strong>(Angular, AWS Lambda, NodeTs)</strong>.',
-                            'Managment & Development of multiple Shopify stores <strong>AlpineJs , Shopify APIs, NodeJs, BOLD API...</strong>.',
-                            'Making AMP Responsive Templates for a website builder platform.',
-                            'APIs integration for a website builder platform <strong>(Google Analytic, Search Console, Lighthouse, Google adsense, Stripe, Rank Tracker ...)</strong>.',
-                        ]
-                    },
-                    {
-                        timeperiod: '2017 → 08/2019',
-                        title: 'SOMAGEC',
-                        descriptions: [
-                            'Creation of Mobile Applications with <strong>Xamarin</strong> and <strong>Java</strong>.',
-                            'Creation of Web Applications with <strong>Core.NET</strong> and <strong>Angular</strong>.',
-                        ]
-                    },
-                    {
-                        timeperiod: 2016,
-                        title: 'Ministry Of Finance',
-                        descriptions: [
-                            'Conception and Development of a Management Application.',
-                            'Creation of <strong>ASP.NET</strong> Web Application Using MVC and Entity Framework.',
-                        ]
-                    }
-                ]
-            },
-            // {
-            //     right: true,
-            //     title: 'PROJECTS',
-            //     items: [
-            //         {
-            //             timeperiod: '2020',
-            //             title: '7imyah.com',
-            //             descriptions: [
-            //                 'Setup and Deploy Website in a <strong>Docker</strong> environment.',
-            //                 'Creation of an Interactive Dashboards in <strong>Angular</strong>.',
-            //                 'Creation of the web interface with <strong>AMP</strong> technology, <u><a href="http://7imyah.com/">7imyah</a></u>',
-            //                 'Creation of the web API with <strong>Node Js</strong>.',
-            //                 'Project conception and development using <strong>MEAN</strong> stack.',
-            //             ]
-            //         }
-            //     ]
-            // },
-            {
-                right: false,
-                title: 'STUDIES',
-                items: [
-                    {
-                        timeperiod: 2022,
-                        title: 'Information Systems Engineering',
-                        descriptions: [
-                            'Database administrator.',
-                            'IS Project Manager.',
-                            'IS study and development consultant.'
-                        ]
-                    },
-                    {
-                        timeperiod: 2020,
-                        title: 'Computer System Engineering',
-                        descriptions: [
-                            'Mastery of statistical and data analysis methods.',
-                            'Participate in data engineering project for decision making.'
-                        ]
-                    },
-                    {
-                        timeperiod: 2016,
-                        title: 'Specialized Technician Diploma',
-                        descriptions: ['Specialized Technician Diploma in Software Development.']
-                    },{
-                        timeperiod: 2014,
-                        title: 'Scientific Baccalaureate',
-                        descriptions: ['High-school Diploma in Maths']
-                    }
-                ]
+              timeperiod: "08/2019 → CURRENT",
+              title: "DataFlex",
+              descriptions: [
+                'Remote Full Stack Developer for <u><a href="https://qrmenu.com/">QRmenu</a></u> <strong>(Angular, AWS Lambda, NodeTs)</strong>.',
+                'Remote Full Stack Developer for <u><a href="https://dopweb.com/">Dopweb</a></u> <strong>(Angular, AWS Lambda, NodeTs)</strong>.',
+                "Managment & Development of multiple Shopify stores <strong>AlpineJs , Shopify APIs, NodeJs, BOLD API...</strong>.",
+                "Making AMP Responsive Templates for a website builder platform.",
+                "APIs integration for a website builder platform <strong>(Google Analytic, Search Console, Lighthouse, Google adsense, Stripe, Rank Tracker ...)</strong>.",
+              ],
             },
             {
-                right: false,
-                title: 'CERTIFICATIONS',
-                items: [
-                    {
-                        timeperiod: 2016,
-                        title: 'Microsoft® Specialist',
-                        descriptions: ['Microsoft® Specialist: Programming in HTML5 with JavaScript and CSS3.']
-                    }, {
-                        timeperiod: 2016,
-                        title: 'Microsoft® Specialist',
-                        descriptions: ['Microsoft® Specialist: Programming in C#.']
-                    }
-                ]
+              timeperiod: "2017 → 08/2019",
+              title: "SOMAGEC",
+              descriptions: [
+                "Creation of Mobile Applications with <strong>Xamarin</strong> and <strong>Java</strong>.",
+                "Creation of Web Applications with <strong>Core.NET</strong> and <strong>Angular</strong>.",
+              ],
             },
             {
-                right: true,
-                title: 'SKILLS',
-                items: [
-                    {
-                        title: 'Teamwork and autonomy',
-                        descriptions: ['Git, TFS, Jira, Trello, Asana']
-                    }, {
-                        title: 'Technologies',
-                        descriptions: ['AWS services, C#, TypeScript, JavaScript', 'Stitches, AMP, REST, GraphQL ...'
-]
-                    }, {
-                        title: 'Frameworks',
-                        descriptions: ['Angular, VueJs, ReactJs, AlpineJs, ASP.NET Core']
-                    }, {
-                        title: 'DataBase',
-                        descriptions: ['SQL Server, PostgreSQL, MongoDB, DynamoDB']
-                    }, {
-                        title: 'DevOps and CI',
-                        descriptions: ['Docker, Vagrant, Travis CI']
-                    }, {
-                        title: 'Servers',
-                        descriptions: ['Windows Server, AWS']
-                    }
-                ]
+              timeperiod: 2016,
+              title: "Ministry Of Finance",
+              descriptions: [
+                "Conception and Development of a Management Application.",
+                "Creation of <strong>ASP.NET</strong> Web Application Using MVC and Entity Framework.",
+              ],
+            },
+          ],
+        },
+        // {
+        //     right: true,
+        //     title: 'PROJECTS',
+        //     items: [
+        //         {
+        //             timeperiod: '2020',
+        //             title: '7imyah.com',
+        //             descriptions: [
+        //                 'Setup and Deploy Website in a <strong>Docker</strong> environment.',
+        //                 'Creation of an Interactive Dashboards in <strong>Angular</strong>.',
+        //                 'Creation of the web interface with <strong>AMP</strong> technology, <u><a href="http://7imyah.com/">7imyah</a></u>',
+        //                 'Creation of the web API with <strong>Node Js</strong>.',
+        //                 'Project conception and development using <strong>MEAN</strong> stack.',
+        //             ]
+        //         }
+        //     ]
+        // },
+        {
+          right: false,
+          title: "STUDIES",
+          items: [
+            {
+              timeperiod: 2022,
+              title: "Information Systems Engineering",
+              descriptions: [
+                "Database administrator.",
+                "IS Project Manager.",
+                "IS study and development consultant.",
+              ],
             },
             {
-                right: true,
-                title: 'EXPERTISE',
-                items: [
-                    {
-                        title: 'Test Driven Development',
-                    }, {
-                        title: 'Rest API Development',
-                    }, {
-                        title: 'APIs Integration',
-                    }, {
-                        title: 'Shopify Store Development',
-                    },{
-                        title: 'Database Management',
-                    }
-                ]
+              timeperiod: 2020,
+              title: "Computer System Engineering",
+              descriptions: [
+                "Mastery of statistical and data analysis methods.",
+                "Participate in data engineering project for decision making.",
+              ],
             },
-        ]
-    }
-  }
-}
+            {
+              timeperiod: 2016,
+              title: "Specialized Technician Diploma",
+              descriptions: [
+                "Specialized Technician Diploma in Software Development.",
+              ],
+            },
+            {
+              timeperiod: 2014,
+              title: "Scientific Baccalaureate",
+              descriptions: ["High-school Diploma in Maths"],
+            },
+          ],
+        },
+        {
+          right: false,
+          title: "CERTIFICATIONS",
+          items: [
+            {
+              timeperiod: 2016,
+              title: "Microsoft® Specialist",
+              descriptions: [
+                "Microsoft® Specialist: Programming in HTML5 with JavaScript and CSS3.",
+              ],
+            },
+            {
+              timeperiod: 2016,
+              title: "Microsoft® Specialist",
+              descriptions: ["Microsoft® Specialist: Programming in C#."],
+            },
+          ],
+        },
+        {
+          right: true,
+          title: "SKILLS",
+          items: [
+            {
+              title: "Teamwork and autonomy",
+              descriptions: ["Git, TFS, Jira, Trello, Asana"],
+            },
+            {
+              title: "Technologies",
+              descriptions: [
+                "AWS services, C#, TypeScript, JavaScript",
+                "Stitches, AMP, REST, GraphQL ...",
+              ],
+            },
+            {
+              title: "Frameworks",
+              descriptions: ["Angular, VueJs, ReactJs, AlpineJs, ASP.NET Core"],
+            },
+            {
+              title: "DataBase",
+              descriptions: ["SQL Server, PostgreSQL, MongoDB, DynamoDB"],
+            },
+            {
+              title: "DevOps and CI",
+              descriptions: ["Docker, Vagrant, Travis CI"],
+            },
+            {
+              title: "Servers",
+              descriptions: ["Windows Server, AWS"],
+            },
+          ],
+        },
+        {
+          right: true,
+          title: "EXPERTISE",
+          items: [
+            {
+              title: "Test Driven Development",
+            },
+            {
+              title: "Rest API Development",
+            },
+            {
+              title: "APIs Integration",
+            },
+            {
+              title: "Shopify Store Development",
+            },
+            {
+              title: "Database Management",
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
 </script>
